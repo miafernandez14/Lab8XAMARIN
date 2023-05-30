a064bae712eea121b7e68c7837890e7e7f5e9a49
miafernandez14 <mia.fernandez@tecsup.edu.pe>
2023-05-29 21:25:06 -0500

    first commit

---
+++ Demo082023A/Demo082023A/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo082023A
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            //MainPage = new ExplicitStyles();
+            //MainPage = new ImplicitStyles();
+            //MainPage = new GlobalStyles();
+            //MainPage = new StyleInheritance();
+            //MainPage = new DinamicStyles();
+            MainPage = new CascadingStyleSheets();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Demo082023A/Demo082023A/DinamicStyles.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Demo082023A
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DinamicStyles : ContentPage
+    {
+        bool originalStyle = true;
+        public DinamicStyles()
+        {
+
+            InitializeComponent();
+            Resources["searchBarStyle"] = Resources["redSearchBarStyle"];
+        }
+        void OnButtonClicked(object sender, EventArgs e)
+        {
+            if (originalStyle)
+            {
+                Resources["searchBarStyle"] = Resources["limeSearchBarStyle"];
+                originalStyle = false;
+            }
+            else
+            {
+                Resources["searchBarStyle"] = Resources["redSearchBarStyle"];
+                originalStyle = true;
+            }
+        }
+    }
+}
